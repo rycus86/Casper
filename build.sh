05ce5036d6cf1ae1e679fd58a951e9e815559382
Viktor Adam <rycus86@gmail.com>
2017-12-15 23:45:45 +0000

    Style changes

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 docker run --rm -it --name ghost-builder    \
     -v $PWD:/app -w /app                    \
     node:alpine                             \
-    sh -c 'npm install && node ./node_modules/gulp/bin/gulp.js zip'
+    sh -c 'if [ ! -d node_modules ]; then npm install; fi && node ./node_modules/gulp/bin/gulp.js zip'
