2c0e884f4da2508fe4c113fbaee83d3cfa6ba33c
Viktor Adam <rycus86@gmail.com>
2017-12-18 22:42:43 +0000

    Reading time, comments and more tags

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
+
+TARGET=${1:-zip}
+
 docker run --rm -it --name ghost-builder    \
     -v $PWD:/app -w /app                    \
     node:alpine                             \
-    sh -c 'if [ ! -d node_modules ]; then npm install; fi && node ./node_modules/gulp/bin/gulp.js zip'
+    sh -c "if [ ! -d node_modules ]; then npm install; fi && node ./node_modules/gulp/bin/gulp.js $TARGET"
